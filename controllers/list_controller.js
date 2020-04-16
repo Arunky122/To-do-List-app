bad197c0e2c7d3bbf126cf2a96db6583970c038d
Arunky122 <arunky122@gmail.com>
2020-04-16 22:58:54 +0530

    initial commit

--- controllers/list_controller.js
+++ controllers/list_controller.js
@@ -42,16 +42,27 @@ module.exports.createList = function(req,res){
 
 // Deleteing multiple task from the database
 
-module.exports.DeleteList = function (req, res) {
-    for(let i in req.body){
-      
-       Taskapp.findByIdAndDelete(req.body[i], function (err) {
-        if (err) {
-          console.log('Error in deleting list from Database',err);
-           return;
+module.exports.DeleteList = function(req, res) {
+    let ids = req.body.task;
+    // if single task is to be deleted
+    if (typeof(ids) == "string") {
+        Taskapp.findByIdAndDelete(ids, function(err) {
+            if (err) { 
+                console.log("error in deleting"); 
+                return; 
+            }
+        });
+    } else {   
+        
+        // if multiple task is to be deleted
+        for (let i = 0; i < ids.length; i++) {
+            Taskapp.findByIdAndDelete(ids[i], function (err) {
+                if (err) { 
+                    console.log("error in deleting");
+                    return; 
+                }
+            });
         }
-  
-      })
     }
     return res.redirect('back');
-  }
+};
